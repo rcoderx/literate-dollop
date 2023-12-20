4a53c4fdf80f81c16d168f301f71d65489bf71f9
rcoderx <104567101+rcoderx@users.noreply.github.com>
2023-12-20 04:12:21 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ function submitForm() {
     const form = document.getElementById('registrationForm');
     const formData = new FormData(form);
 
-    fetch('automatic-octo-fortnight-production.up.railway.app/submit', {
+    fetch('automatic-octo-fortnight-production.up.railway.app/submit', { // Replace with your actual backend domain
         method: 'POST',
         body: formData
     })
     .then(response => response.text())
     .then(data => {
-        alert(data); // Display the server's response
+        if(data === 'Registration successful') {
+            const userAddress = document.getElementById('userAddress').value;
+            const referralLink = `https://yourfrontenddomain.com/${userAddress}`; // Replace with your actual frontend domain
+            alert('Registered successfully!\nYour referral link: ' + referralLink);
+        } else {
+            alert(data);
+        }
     })
     .catch(error => {
         console.error('Error:', error);
